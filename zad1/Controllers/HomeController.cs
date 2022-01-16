0af70c4612fc40298865735a6b8a10ade1909ec5
Dawid Dziedzic <62702482+dawiddzie@users.noreply.github.com>
2022-01-16 18:58:30 +0100

    Add project files.

---
+++ zad1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using zad1.Models;
+
+
+namespace zad1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Number")] Dane dane) 
+        {
+            return View(dane);
+        }
+    }
+}
---
+++ zad1/Models/Dane.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace zad1.Models
+{
+    public class Dane
+    {
+        public string Name { get; set; }
+        public DateTime Number { get; set; }
+    }
+}
